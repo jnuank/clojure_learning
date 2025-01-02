9f372550260f533b97e7b9ce0a05b1fce1c57414
jnuank <ikuta1919@gmail.com>
2025-01-02 19:32:35 +0900

    clojureの構文をちょっと触る

--- tutorial.clj
+++ tutorial.clj
@@ -4,3 +4,45 @@
   (if (= n 0)
     1
    	(* n (fanctorial (dec n)))))
+
+
+42N
+
+-1.5
+
+22/7
+
+"hello"
+
+\e
+
+#"[0-9]+"
+
+\newline
+
+\space
+
+" "
+
+"\n"
+
+\tab
+
+map
++
+clojure.core/+
+
+
+true false
+
+:alpha
+
+:release/alpha
+
+'(1 2 3) ;list
+[1 2 3 ] ;vector
+#{1 2 3} ;set
+{:a 1 :b 2} ;map
+
+; ↑これらのリテラルコレクションの違いはなんだろう
+
