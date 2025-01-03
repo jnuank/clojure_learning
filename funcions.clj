f61030293f0c4d49129538e499632ebf5f2870a0
jnuank <ikuta1919@gmail.com>
2025-01-03 23:58:27 +0900

    ちょっとした演習

--- funcions.clj
+++ funcions.clj
@@ -14,7 +14,7 @@
 (hello "greeting" "aa" "bb")
 
 ; 匿名関数
-(fn [message] (println message)) 
+(fn [message] (println message))
 
 ; 省略形
 ; (fn [x] (+ 6 x))
@@ -58,14 +58,71 @@
 
 (apply println '("a" "b" "c"))
 (apply println "a" '("b" "c"))
+(apply + "a" "1" "2" "3" "4" "5" '("b" "c"))
 
 
 (defn plot [shape coords]
   (plotxy shape (first coords) (second coords)))
 
+(list* 1 [3 2])
+
+()
+
 ; letの使い方
 (let [x 1 y 2] (+ x y))
 
 ; letの外側だとエラーとなる
-((let [x 1 y 2] ) (+ x y))
+((let [x 1 y 2]) (+ x y))
+
+; クロージャを作成
+; fnはspecial form→closureを作成する
+(defn messenger-builder [greeting]
+  (fn [who] (println greeting who)))
+
+; greetingに、Helloを渡した匿名関数を定義
+(def hello-er (messenger-builder "Hello"))
+
+; 外側から渡せる
+(hello-er "world")
+
+
+((fn  [message]  (println message) ) "mess")
+(#(println %) "mess")
+
+
+; テスト
+
+; 1
+(defn greeting [] (println "Hello"))
+
+(greeting)
+
+; 2
+(def greet (fn [] (println "Hello")))
+
+(def greet #(println "Hello!"))
+(greet)
+
+; 3
+
+(defn greeting
+  ([] (str "Hello World!"))
+  ([x] (str "Hello" \space x "!"))
+  ([x y] (str x "," \space y "!")))
+
+(assert (= "Hello World!" (greeting)))
+(assert (= "Hello Clojure!" (greeting "Clojure")))
+(assert (= "Good morning, Clojure!" (greeting "Good morning" "Clojure")))
+
+; 4
+
+(defn do-nothing [x]
+  (identity x))
+
+(do-nothing 1)
+
+(defn always-thing [_] 100)
+
+(always-thing "a")
+
 
