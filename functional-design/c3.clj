185a6714d0f9537cb90bfc1d65f5072087bde2b4
jnuank <ikuta1919@gmail.com>
2025-01-05 18:50:21 +0900

    memo化

--- functional-design/c3.clj
+++ functional-design/c3.clj
@@ -57,4 +57,20 @@
 (time (ifib 20))
 
 
-; 
+; memo
+
+(declare fib)
+
+(defn fib-w [n]
+  (cond
+    (< n 1) nil
+   	(<= n 2) 1
+   	:else (+ (fib (dec n)) (fib (- n 2)))))
+
+(def fib (memoize fib-w))
+
+(time (fib 20))
+
+(def memot (memoize fib-w))
+
+(time (memot 1000))
