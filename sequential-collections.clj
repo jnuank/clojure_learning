41b2597e2cd5ee991b23f0be5f42b71c760d4afe
jnuank <ikuta1919@gmail.com>
2025-01-06 15:19:07 +0900

    シーケンシャルコレクション

---
+++ sequential-collections.clj
@@ -0,0 +1,24 @@
+[1 2 3]
+
+(get ["abc" false 99] -2)
+
+
+(count [1 2 3 4 1])
+
+(count [])
+
+(vector 1 2 3)
+
+(conj [1 2 3] 4 5 6)
+
+(def v [1 2 3])
+
+(conj v 4 5 6)
+
+v
+
+(def cards)
+
+; vectorとlist。
+; それぞれどっち使おうって感じか
+
