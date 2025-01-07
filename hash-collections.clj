83b31de99e5f686eaf1f4e7a69cd36e627af08d8
jnuank <ikuta1919@gmail.com>
2025-01-07 23:00:05 +0900

    recordいい

--- hash-collections.clj
+++ hash-collections.clj
@@ -37,7 +37,7 @@ scores
 
 scores
 
-(assoc scores "Sally" 0 )
+(assoc scores "Sally" 0)
 
 (assoc scores "Bob" 0)
 
@@ -51,3 +51,91 @@ scores
 (def bad-lookup-map ["a" "B"])
 
 (bad-lookup-map 1)
+
+(keys scores)
+
+; 2つのシーケンスからkeyとvalueつくるやつ
+players
+(zipmap players (repeat 0))
+
+
+(doc repeat)
+
+(zipmap [1 2 3] '("a" "b" "c" "d"))
+
+(zipmap scores (repeat 0))
+
+scores
+
+(into {} (map (fn [player] [player 0]) players))
+(into {} (map #(vector % 0) players))
+
+(map #(println %) players)
+players
+
+(reduce (fn [m player]
+          (assoc m player 0)
+          {
+           players}))
+
+; 動きがよくわからん reduce
+(reduce (fn [m player] (assoc m player 0)) {} players)
+
+(doc reduce)
+
+
+(def person
+  {:first-name "Kelly"
+   :last-name "Keen"
+   :age 32
+   :occupation "Programmer"})
+
+(get person :occupation)
+
+(person :occupation)
+
+(:occupation person)
+
+; 存在しないkeyの場合はデフォ値も使える
+(:hoge person "default")
+
+; ネストされたmap
+
+(def company
+  {:name "WidgetCo"
+   :address {:street "123 Main St"
+             :city "Springfield" 
+             :state "IL"}})
+
+(get-in company [:address :city])
+
+; record
+(defrecord Person [first-name last-name age occupation])
+
+; ネストはこんな感じ
+(defrecord Address [street city state])
+(defrecord Company [name address])
+
+(->Company)
+
+(def company (->Company "company1"
+                        (->Address "street" "city" "state")))
+
+(def company-other (Company. "company1"
+                        (Address. "street" "city" "state")))
+
+company-other
+
+company
+; これが一つの関数として呼び出せる
+(->Person)
+
+(def kelly (->Person "Kelly" "Keen" 32 "Programmer"))
+
+(:first-name kelly)
+(kelly :first-name) ;これだけエラー
+(get kelly :first-name)
+
+(person :first-name)
+(:first-name person)
+(get person :first-name)
