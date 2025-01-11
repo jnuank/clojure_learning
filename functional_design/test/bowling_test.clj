e1557fd4536076e5359d6a31b12b1b07b3a8ef24
jnuank <ikuta1919@gmail.com>
2025-01-11 20:35:52 +0900

    テスト

--- functional_design/test/bowling_test.clj
+++ functional_design/test/bowling_test.clj
@@ -1,25 +1,29 @@
 (ns functional-design.test.bowling-test
   (:require [clojure.test :as t]))
 
-(defn- to-frames [rolls]
-  (partition 2 rolls))
+(defn to-frames [rolls]
+  (let [frames (partition 2 rolls)
+        possible-bonuses (map #(take 1 %) (rest frames))
+        possible-bonuses (concat possible-bonuses [[0]])]
+    (map concat frames possible-bonuses)))
 
-(defn add-frame [score frame]
-  (+ score (reduce + frame)))
+(defn add-frame [score frame-and-bonus]
+  (let [frame (take 2 frame-and-bonus)]
+    (if (= 10 (reduce + frame))
+      (+ score (reduce + frame-and-bonus))
+      (+ score (reduce + frame)))))
 
 (defn score [rolls]
-	(reduce add-frame 0 (to-frames rolls))
-  )
+  (reduce add-frame 0 (to-frames rolls)))
 
 (t/run-tests)
 
 (t/deftest bowling-score
   (t/testing "オールガーター"
-    (t/is (= 0 (score (take 20 (repeat 0)))))) 
+    (t/is (= 0 (score (take 20 (repeat 0))))))
 
   (t/testing "すべて1ピン"
     (t/is (= 20 (score (take 20 (repeat 1))))))
 
   (t/testing "1回スペア"
-    (t/is (= 24 (score (concat [5 5 7] (take 17 (repeat 0)))))))
- 	)
+    (t/is (= 24 (score (concat [5 5 7] (take 17 (repeat 0))))))))
