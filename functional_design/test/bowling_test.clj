bb8f02213245422e2ce767f07896fe1dbb6cbd4b
jnuank <ikuta1919@gmail.com>
2025-01-12 12:17:08 +0900

    パーフェクトゲーム

--- functional_design/test/bowling_test.clj
+++ functional_design/test/bowling_test.clj
@@ -11,26 +11,18 @@
       (= 10 (first remaining-rolls))
       (recur (rest remaining-rolls) (conj frames (take 3 remaining-rolls)))
 
-      (= 10 (reduce + (take 2 remaining-rolls)) )
+      (= 10 (reduce + (take 2 remaining-rolls)))
       (recur (drop 2 remaining-rolls) (conj frames (take 3 remaining-rolls)))
 
       :else
-   	  (recur (drop 2 remaining-rolls) (conj frames (take 2 remaining-rolls))))))
-
-(first [10 2 3])
-(take 1 [10 2 3])
-
-(= 10 (first  [10 2 3]))
-
-(rest [10 2 3])
-(drop 1 [10 2 3])
+      (recur (drop 2 remaining-rolls) (conj frames (take 2 remaining-rolls))))))
 
 ; フレームごとにただた計算するだけ
 (defn frame-score [score frame-and-bonus]
   (+ score (reduce + frame-and-bonus)))
 
 (defn score [rolls]
-  (reduce frame-score 0 (to-frames rolls)))
+  (reduce frame-score 0 (take 10 (to-frames rolls))))
 
 (t/run-tests)
 
@@ -45,4 +37,7 @@
     (t/is (= 24 (score (concat [5 5 7] (take 17 (repeat 0)))))))
 
   (t/testing "1回ストライク"
-    (t/is (= 20 (score (concat [10 2 3] (take 16 (repeat 0))))))))
+    (t/is (= 20 (score (concat [10 2 3] (take 16 (repeat 0)))))))
+
+  (t/testing "パーフェクトゲーム"
+    (t/is (= 300 (score (take 12 (repeat 10)))))))
