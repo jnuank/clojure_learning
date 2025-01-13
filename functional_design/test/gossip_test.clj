db4b2998d765a29f7d09ee922ab781ac13755ac4
jnuank <ikuta1919@gmail.com>
2025-01-13 17:36:14 +0900

    make-driver

--- functional_design/test/gossip_test.clj
+++ functional_design/test/gossip_test.clj
@@ -1,8 +1,15 @@
 (ns functional-design.test.gossip-test
   (:require [clojure.test :as t]))
 
+(defn make-driver [name]
+  {:name name})
+
 (t/run-tests)
 
 (t/deftest gossip-test
   (t/testing "とりあえず"
-  (t/is (= 2 1))))
+    (t/is (= 1 1)))
+
+ 	(t/testing "driverつくる"
+   	(t/is (= {:name "driver1"} (make-driver "driver1")))))
+
