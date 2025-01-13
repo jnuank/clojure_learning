03fe7d2c9744caf6ba624689e9eb214204417978
jnuank <ikuta1919@gmail.com>
2025-01-13 22:59:50 +0900

    group-byをマスターしたっぽいぞ

--- functional_design/src/gossip.clj
+++ functional_design/src/gossip.clj
@@ -11,14 +11,17 @@
 (defn drive [drivers]
   (map #(drive-route %) drivers))
 
- (-> (make-driver "driver2" [:s1 :s2 :s3] #{:r1})
-     drive-route)
+(-> (make-driver "driver2" [:s1 :s2 :s3] #{:r1})
+    drive-route)
 
 (let [driver1 (make-driver "driver1" [:s1 :s2 :s3] #{:r1})
       driver2 (make-driver "driver2" [:s2 :s3 :s4] #{:r1})]
   (drive [driver1 driver2]))
 
 (defn get-stops [drivers]
-     {:stop1 [{:name "driver1" :route [:stop1]}
-                   {:name "driver2" :route [:stop1]}]
-           :stop2 [{:name "driver3" :route [:stop2]}]})
+  (->> drivers
+       (sort-by :name)
+       ; group-by f coll
+       ; driversの各要素(driver)に対して匿名関数を適用した結果をキーとして、 
+       ; collに対してグループ化する
+       (group-by #(first (:route %)))))
--- functional_design/test/gossip_test.clj
+++ functional_design/test/gossip_test.clj
@@ -6,30 +6,28 @@
 
 (t/deftest gossip-test
   (t/testing "driverつくる"
-    (t/is (= {:name "driver1" :route [:stop1] :rumors #{:rumors1} }
-             (make-driver "driver1" [:stop1] #{:rumors1 }))))
+    (t/is (= {:name "driver1" :route [:stop1] :rumors #{:rumors1}}
+             (make-driver "driver1" [:stop1] #{:rumors1}))))
 
   (t/testing "ドライバーの1つ目に停まるバス停1"
     (let [driver (make-driver "driver1" [:stop1] #{:rumors1})
           world [driver]
           new-world (drive world)]
       (t/is (= 1 (count new-world)))
-      (t/is (= :stop1 (-> new-world first :route first)))
-      ))
-  
+      (t/is (= :stop1 (-> new-world first :route first)))))
+
   (t/testing "ドライバーの2つ目に停まるバス停2"
     (let [driver (make-driver "driver1" [:stop1 :stop2] #{:rumors1})
           world [driver]
           new-world (drive world)]
       (t/is (= 1 (count new-world)))
-      (t/is (= :stop2 (-> new-world first :route first)))
-      ))
-  
+      (t/is (= :stop2 (-> new-world first :route first)))))
+
   (t/testing "二人のドライバーが停まるバス停"
     (let [driver1 (make-driver "driver1" [:stop1 :stop2] #{:rumors1})
           driver2 (make-driver "driver2" [:stop1 :stop3 :stop2] #{:rumors2})
           world [driver1 driver2]
-          new-world1 (drive world) 
+          new-world1 (drive world)
           new-world2 (drive new-world1)]
       (t/is (= 2 (count new-world1)))
       (t/is (= :stop2 (-> new-world1 first :route first)))
@@ -37,14 +35,12 @@
       (t/is (= 2 (count new-world2)))
       (t/is (= :stop1 (-> new-world2 first :route first)))
       (t/is (= :stop2 (-> new-world2 second :route first)))))
-  
+
   (t/testing "バス停取得"
-    
-
-(let [drivers []]
-  (t/is (= {:stop1 [{:name "driver1" :route [:stop1]}
-                   {:name "driver2" :route [:stop1]}]
-           :stop2 [{:name "driver3" :route [:stop2]}]}
-          (get-stops drivers))))))
-
-
+    (let [drivers #{{:name "driver1" :route [:stop1]}
+                    {:name "driver2" :route [:stop1]}
+                    {:name "driver3" :route [:stop2]}}]
+      (t/is (= {:stop1 [{:name "driver1" :route [:stop1]}
+                        {:name "driver2" :route [:stop1]}]
+                :stop2 [{:name "driver3" :route [:stop2]}]}
+               (get-stops drivers))))))
