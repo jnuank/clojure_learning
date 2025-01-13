89d74bb015685d277e551a17f272933ccc4c9e73
jnuank <ikuta1919@gmail.com>
2025-01-13 18:53:14 +0900

    drive2回したときの検証

--- functional_design/test/gossip_test.clj
+++ functional_design/test/gossip_test.clj
@@ -29,9 +29,13 @@
     (let [driver1 (make-driver "driver1" [:stop1 :stop2] #{:rumors1})
           driver2 (make-driver "driver2" [:stop1 :stop3 :stop2] #{:rumors2})
           world [driver1 driver2]
-          new-world1 (drive world)]
+          new-world1 (drive world)
+          new-world2 (drive new-world1)]
       (t/is (= 2 (count new-world1)))
       (t/is (= :stop2 (-> new-world1 first :route first)))
-      (t/is (= :stop3 (-> new-world1 second :route first))))))
+      (t/is (= :stop3 (-> new-world1 second :route first)))
+      (t/is (= 2 (count new-world2)))
+      (t/is (= :stop1 (-> new-world2 first :route first)))
+      (t/is (= :stop2 (-> new-world2 second :route first))))))
 
 
