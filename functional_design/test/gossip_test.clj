b4bc640d478895f6baaeb70116db08c87de958a4
jnuank <ikuta1919@gmail.com>
2025-01-13 23:05:45 +0900

    アサート・ファースト

--- functional_design/test/gossip_test.clj
+++ functional_design/test/gossip_test.clj
@@ -43,4 +43,10 @@
       (t/is (= {:stop1 [{:name "driver1" :route [:stop1]}
                         {:name "driver2" :route [:stop1]}]
                 :stop2 [{:name "driver3" :route [:stop2]}]}
-               (get-stops drivers))))))
+               (get-stops drivers)))))
+
+  (t/testing "噂話をする"
+    (t/is (= [{:name "driver1" :rumors #{:rumor2 :rumor1}}
+              {:name "driver2" :rumors #{:rumor1 :rumor2}}]
+             (merge-rumors []))))
+  )
