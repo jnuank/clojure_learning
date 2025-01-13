b21cec9a1acfd0543fa24da7d41a00667a372bd1
jnuank <ikuta1919@gmail.com>
2025-01-13 18:17:33 +0900

    バス停のやつ

--- functional_design/src/gossip.clj
+++ functional_design/src/gossip.clj
@@ -3,3 +3,7 @@
 
 (defn make-driver [name route rumors]
   {:name name :route route :rumors rumors})
+
+(defn drive [drivers]
+  (println drivers)
+  drivers)
--- functional_design/test/gossip_test.clj
+++ functional_design/test/gossip_test.clj
@@ -1,19 +1,28 @@
 (ns functional-design.test.gossip-test
   (:require [clojure.test :as t]
-            [functional-design.src.gossip :refer [make-driver]]))
+            [functional-design.src.gossip :refer :all]))
 
 (t/run-tests)
 
 (t/deftest gossip-test
-  (t/testing "とりあえず"
-    (t/is (= 1 1)))
-
   (t/testing "driverつくる"
     (t/is (= {:name "driver1" :route [:stop1] :rumors #{:rumors1} }
-             (make-driver "driver1" [:stop1] #{:rumors1 })))))
+             (make-driver "driver1" [:stop1] #{:rumors1 }))))
+
+  (t/testing "ドライバーは次のバス停に停まる"
+    (let [driver (make-driver "driver1" [:stop1] #{:rumors1})
+          world [driver]
+          new-world (drive world)]
+      (t/is (= 1 (count new-world)))
+      (t/is (= :stop1 (-> new-world first :route first)))
+      )))
 
 
-[:s1]
 
 ; driverはnameとrouteとrumorsを持っている
 
+(-> (make-driver "driver1" [:s1 :s2 :s3] #{:r1})
+    vector
+    drive first :route first)
+
+(= :s1 "aa")
