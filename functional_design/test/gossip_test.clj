d1862fe166804963695063a5c705a3ff8c255c2f
jnuank <ikuta1919@gmail.com>
2025-01-13 18:59:33 +0900

    仮実装

--- functional_design/test/gossip_test.clj
+++ functional_design/test/gossip_test.clj
@@ -29,13 +29,22 @@
     (let [driver1 (make-driver "driver1" [:stop1 :stop2] #{:rumors1})
           driver2 (make-driver "driver2" [:stop1 :stop3 :stop2] #{:rumors2})
           world [driver1 driver2]
-          new-world1 (drive world)
+          new-world1 (drive world) 
           new-world2 (drive new-world1)]
       (t/is (= 2 (count new-world1)))
       (t/is (= :stop2 (-> new-world1 first :route first)))
       (t/is (= :stop3 (-> new-world1 second :route first)))
       (t/is (= 2 (count new-world2)))
       (t/is (= :stop1 (-> new-world2 first :route first)))
-      (t/is (= :stop2 (-> new-world2 second :route first))))))
+      (t/is (= :stop2 (-> new-world2 second :route first)))))
+  
+  (t/testing "バス停取得"
+    
+
+(let [drivers []]
+  (t/is (= {:stop1 [{:name "driver1" :route [:stop1]}
+                   {:name "driver2" :route [:stop1]}]
+           :stop2 [{:name "driver3" :route [:stop2]}]}
+          (get-stops drivers))))))
 
 
