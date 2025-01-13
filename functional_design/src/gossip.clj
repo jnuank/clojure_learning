5de997774e02269dd82358061481821245c707f6
jnuank <ikuta1919@gmail.com>
2025-01-13 18:50:20 +0900

    drive関数つくる

--- functional_design/src/gossip.clj
+++ functional_design/src/gossip.clj
@@ -4,6 +4,16 @@
 (defn make-driver [name route rumors]
   {:name name :route route :rumors rumors})
 
+(defn- drive-route [driver]
+  (let [new-route (vec (concat (rest (:route driver)) [(first (:route driver))]))]
+    (assoc driver :route new-route)))
+
 (defn drive [drivers]
-  (println drivers)
-  drivers)
+  (map #(drive-route %) drivers))
+
+ (-> (make-driver "driver2" [:s1 :s2 :s3] #{:r1})
+     drive-route)
+
+(let [driver1 (make-driver "driver1" [:s1 :s2 :s3] #{:r1})
+      driver2 (make-driver "driver2" [:s2 :s3 :s4] #{:r1})]
+  (drive [driver1 driver2]))
--- functional_design/test/gossip_test.clj
+++ functional_design/test/gossip_test.clj
@@ -9,20 +9,29 @@
     (t/is (= {:name "driver1" :route [:stop1] :rumors #{:rumors1} }
              (make-driver "driver1" [:stop1] #{:rumors1 }))))
 
-  (t/testing "ドライバーは次のバス停に停まる"
+  (t/testing "ドライバーの1つ目に停まるバス停1"
     (let [driver (make-driver "driver1" [:stop1] #{:rumors1})
           world [driver]
           new-world (drive world)]
       (t/is (= 1 (count new-world)))
       (t/is (= :stop1 (-> new-world first :route first)))
-      )))
+      ))
+  
+  (t/testing "ドライバーの2つ目に停まるバス停2"
+    (let [driver (make-driver "driver1" [:stop1 :stop2] #{:rumors1})
+          world [driver]
+          new-world (drive world)]
+      (t/is (= 1 (count new-world)))
+      (t/is (= :stop2 (-> new-world first :route first)))
+      ))
+  
+  (t/testing "二人のドライバーが停まるバス停"
+    (let [driver1 (make-driver "driver1" [:stop1 :stop2] #{:rumors1})
+          driver2 (make-driver "driver2" [:stop1 :stop3 :stop2] #{:rumors2})
+          world [driver1 driver2]
+          new-world1 (drive world)]
+      (t/is (= 2 (count new-world1)))
+      (t/is (= :stop2 (-> new-world1 first :route first)))
+      (t/is (= :stop3 (-> new-world1 second :route first))))))
 
 
-
-; driverはnameとrouteとrumorsを持っている
-
-(-> (make-driver "driver1" [:s1 :s2 :s3] #{:r1})
-    vector
-    drive first :route first)
-
-(= :s1 "aa")
